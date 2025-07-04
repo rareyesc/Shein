f4afa09367e1ffbbb7c383696efa38e74dcff419
rareyesc <109057095+rareyesc@users.noreply.github.com>
2025-07-04 14:27:24 -0500

    Expand README with usage and docs

--- micro_genero/src/test/java/shein/micro_genero/MicroGeneroApplicationTests.java
+++ micro_genero/src/test/java/shein/micro_genero/MicroGeneroApplicationTests.java
@@ -1,10 +1,10 @@
-package shein.micro_productos;
+package shein.micro_genero;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroPedidoApplicationTests {
+class MicroGeneroApplicationTests {
     @Test
     void contextLoads() {}
 }
